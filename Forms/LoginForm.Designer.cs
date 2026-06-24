46c465e9da29702e1b60edb2913a010574a6c558
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2026-06-24 22:47:14 +0530

    Designed Manage Orders Form

--- Forms/LoginForm.Designer.cs
+++ Forms/LoginForm.Designer.cs
@@ -31,8 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
             this.LoginFormTitle2 = new System.Windows.Forms.Label();
             this.LoginFormTitle1 = new System.Windows.Forms.Label();
-            this.labelEmail = new System.Windows.Forms.Label();
-            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.labelMobileNumber = new System.Windows.Forms.Label();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.labelPassword = new System.Windows.Forms.Label();
             this.checkBoxRole = new System.Windows.Forms.CheckBox();
@@ -64,26 +64,26 @@
             this.LoginFormTitle1.TabIndex = 1;
             this.LoginFormTitle1.Text = "Welcome Back";
             // 
-            // labelEmail
+            // labelMobileNumber
             // 
-            this.labelEmail.AutoSize = true;
-            this.labelEmail.BackColor = System.Drawing.Color.Transparent;
-            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelEmail.Location = new System.Drawing.Point(50, 136);
-            this.labelEmail.Name = "labelEmail";
-            this.labelEmail.Size = new System.Drawing.Size(52, 17);
-            this.labelEmail.TabIndex = 3;
-            this.labelEmail.Text = "Email ";
-            this.labelEmail.Click += new System.EventHandler(this.label4_Click);
+            this.labelMobileNumber.AutoSize = true;
+            this.labelMobileNumber.BackColor = System.Drawing.Color.Transparent;
+            this.labelMobileNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMobileNumber.Location = new System.Drawing.Point(50, 136);
+            this.labelMobileNumber.Name = "labelMobileNumber";
+            this.labelMobileNumber.Size = new System.Drawing.Size(116, 17);
+            this.labelMobileNumber.TabIndex = 3;
+            this.labelMobileNumber.Text = "Mobile Number";
+            this.labelMobileNumber.Click += new System.EventHandler(this.label4_Click);
             // 
-            // txtEmail
+            // txtMobileNumber
             // 
-            this.txtEmail.BackColor = System.Drawing.SystemColors.Info;
-            this.txtEmail.Location = new System.Drawing.Point(52, 157);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(235, 20);
-            this.txtEmail.TabIndex = 4;
-            this.txtEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtMobileNumber.BackColor = System.Drawing.SystemColors.Info;
+            this.txtMobileNumber.Location = new System.Drawing.Point(52, 157);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(235, 20);
+            this.txtMobileNumber.TabIndex = 4;
+            this.txtMobileNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txtPassword
             // 
@@ -166,8 +166,8 @@
             this.Controls.Add(this.checkBoxRole);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.labelPassword);
-            this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.txtMobileNumber);
+            this.Controls.Add(this.labelMobileNumber);
             this.Controls.Add(this.LoginFormTitle1);
             this.Controls.Add(this.LoginFormTitle2);
             this.Name = "LoginForm";
@@ -183,8 +183,8 @@
 
         private System.Windows.Forms.Label LoginFormTitle2;
         private System.Windows.Forms.Label LoginFormTitle1;
-        private System.Windows.Forms.Label labelEmail;
-        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label labelMobileNumber;
+        private System.Windows.Forms.TextBox txtMobileNumber;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Label labelPassword;
         private System.Windows.Forms.CheckBox checkBoxRole;
