83378351614f028f81db9fce7914e66e7dcd5017
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2026-06-24 10:03:41 +0530

    Designed Register From

---
+++ Forms/RegisterForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SmartMedPharmacy.Forms
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegisterForm));
+            this.RegisterFormTitle = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.txtMobileNumber = new System.Windows.Forms.TextBox();
+            this.labelMobileNumber = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.lnkSignIn = new System.Windows.Forms.LinkLabel();
+            this.labelSignIn = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RegisterFormTitle
+            // 
+            this.RegisterFormTitle.AutoSize = true;
+            this.RegisterFormTitle.BackColor = System.Drawing.Color.Transparent;
+            this.RegisterFormTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterFormTitle.Location = new System.Drawing.Point(54, 37);
+            this.RegisterFormTitle.Name = "RegisterFormTitle";
+            this.RegisterFormTitle.Size = new System.Drawing.Size(160, 24);
+            this.RegisterFormTitle.TabIndex = 0;
+            this.RegisterFormTitle.Text = "Create Account ";
+            this.RegisterFormTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.SystemColors.Info;
+            this.txtEmail.Location = new System.Drawing.Point(58, 110);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(235, 20);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.BackColor = System.Drawing.Color.Transparent;
+            this.labelEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEmail.Location = new System.Drawing.Point(56, 89);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(52, 17);
+            this.labelEmail.TabIndex = 5;
+            this.labelEmail.Text = "Email ";
+            // 
+            // txtMobileNumber
+            // 
+            this.txtMobileNumber.BackColor = System.Drawing.SystemColors.Info;
+            this.txtMobileNumber.Location = new System.Drawing.Point(58, 159);
+            this.txtMobileNumber.Name = "txtMobileNumber";
+            this.txtMobileNumber.Size = new System.Drawing.Size(235, 20);
+            this.txtMobileNumber.TabIndex = 8;
+            this.txtMobileNumber.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelMobileNumber
+            // 
+            this.labelMobileNumber.AutoSize = true;
+            this.labelMobileNumber.BackColor = System.Drawing.Color.Transparent;
+            this.labelMobileNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelMobileNumber.Location = new System.Drawing.Point(56, 138);
+            this.labelMobileNumber.Name = "labelMobileNumber";
+            this.labelMobileNumber.Size = new System.Drawing.Size(116, 17);
+            this.labelMobileNumber.TabIndex = 7;
+            this.labelMobileNumber.Text = "Mobile Number";
+            this.labelMobileNumber.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.Info;
+            this.txtPassword.Location = new System.Drawing.Point(59, 262);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(235, 20);
+            this.txtPassword.TabIndex = 10;
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.BackColor = System.Drawing.Color.Transparent;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(57, 241);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(77, 17);
+            this.labelPassword.TabIndex = 9;
+            this.labelPassword.Text = "Password";
+            this.labelPassword.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.BackColor = System.Drawing.SystemColors.Info;
+            this.txtAddress.Location = new System.Drawing.Point(59, 210);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(235, 20);
+            this.txtAddress.TabIndex = 12;
+            this.txtAddress.TextChanged += new System.EventHandler(this.textBox1_TextChanged_2);
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.BackColor = System.Drawing.Color.Transparent;
+            this.labelAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAddress.Location = new System.Drawing.Point(57, 189);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(67, 17);
+            this.labelAddress.TabIndex = 11;
+            this.labelAddress.Text = "Address";
+            this.labelAddress.Click += new System.EventHandler(this.label1_Click_3);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegister.ForeColor = System.Drawing.Color.Snow;
+            this.btnRegister.Location = new System.Drawing.Point(59, 305);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(233, 36);
+            this.btnRegister.TabIndex = 13;
+            this.btnRegister.Text = "REGISTER";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // lnkSignIn
+            // 
+            this.lnkSignIn.AutoSize = true;
+            this.lnkSignIn.BackColor = System.Drawing.Color.Transparent;
+            this.lnkSignIn.Location = new System.Drawing.Point(216, 346);
+            this.lnkSignIn.Name = "lnkSignIn";
+            this.lnkSignIn.Size = new System.Drawing.Size(40, 13);
+            this.lnkSignIn.TabIndex = 15;
+            this.lnkSignIn.TabStop = true;
+            this.lnkSignIn.Text = "Sign In";
+            // 
+            // labelSignIn
+            // 
+            this.labelSignIn.BackColor = System.Drawing.Color.Transparent;
+            this.labelSignIn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.labelSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSignIn.Location = new System.Drawing.Point(84, 346);
+            this.labelSignIn.Name = "labelSignIn";
+            this.labelSignIn.Size = new System.Drawing.Size(171, 23);
+            this.labelSignIn.TabIndex = 14;
+            this.labelSignIn.Text = "Already have an Account ?";
+            // 
+            // RegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lnkSignIn);
+            this.Controls.Add(this.labelSignIn);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.txtMobileNumber);
+            this.Controls.Add(this.labelMobileNumber);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.RegisterFormTitle);
+            this.Name = "RegisterForm";
+            this.Text = "RegisterForm";
+            this.Load += new System.EventHandler(this.RegisterForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label RegisterFormTitle;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.TextBox txtMobileNumber;
+        private System.Windows.Forms.Label labelMobileNumber;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.LinkLabel lnkSignIn;
+        private System.Windows.Forms.Label labelSignIn;
+    }
+}
---
+++ Forms/RegisterForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmartMedPharmacy.Forms
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
