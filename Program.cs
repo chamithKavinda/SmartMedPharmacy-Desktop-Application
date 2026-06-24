72513a51f95693484147bedd1edf3b77ac5db0b7
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2026-06-24 14:13:17 +0530

    Designed AdminPannels

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace SmartMedPharmacy
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SmartMedPharmacy.Forms.RegisterForm());
         }
     }
 }
