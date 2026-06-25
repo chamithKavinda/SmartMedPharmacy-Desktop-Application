ad5bd5d44727c3dc438780dea6e43f41602716de
chamithKavinda <139870167+chamithKavinda@users.noreply.github.com>
2026-06-25 15:59:01 +0530

    User Model Implemented

---
+++ Utill/PasswordHasher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SmartMedPharmacy.Utill
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(
+                    Encoding.UTF8.GetBytes(password)
+                );
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (byte b in bytes)
+                    sb.Append(b.ToString("x2"));
+
+                return sb.ToString();
+            }
+        }
+
+        public static bool VerifyPassword(string inputPassword, string storedHash)
+        {
+            return HashPassword(inputPassword) == storedHash;
+        }
+    }
+}
